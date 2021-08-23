017f7f33aef9cec5f6541c23436a108bdc5a28ec
kjdchapman <kjdchapman@gmail.com>
2021-08-23 13:31:08 +0100

    WIP add first part of basic acceptance test

--- spec/defusing_a_simple_bomb_card_spec.rb
+++ spec/defusing_a_simple_bomb_card_spec.rb
@@ -1,3 +1,12 @@
 describe 'defusing a simple bomb card' do
+  it 'looks right' do
+    deck = Deck.new
 
+    first_card = deck.deal
+
+    expect(first_card.boxes.first).to eq({ number: 2, colour: nil })
+  end
+
+  it 'can have a dice placed on it' do
+  end
 end
